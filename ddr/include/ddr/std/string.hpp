7e627b1e1bfb5f7df8dbf274e234d05148aceaa6
Gaurav Chaudhari <gaurav.chaudhari@ibm.com>
2024-07-18 11:04:04 -0400

    Remove macro redefinitions that cause issues with Open XL
    Couple instances where the toupper() and tolower() macros
    are undef and re-defined that cause problems with the
    native headers. These may be unnecessary and are removed.
    
    Signed-off-by: Gaurav Chaudhari <gaurav.chaudhari@ibm.com>

--- ddr/include/ddr/std/string.hpp
+++ ddr/include/ddr/std/string.hpp
@@ -27,7 +27,16 @@
 #if defined(J9ZOS390)
 
 #include <ctype.h>
+#if !defined(__open_xl__)
+#undef toupper
+#undef tolower
+#endif /* !defined(__open_xl__) */
 #include <string>
+
+#if !defined(__open_xl__)
+#define toupper(c) (islower(c) ? (c & _XUPPER_ASCII) : c)
+#define tolower(c) (isupper(c) ? (c | _XLOWER_ASCII) : c)
+#endif /* defined(__open_xl__) */
 
 #else /* defined(J9ZOS390) */
 #include <string>
